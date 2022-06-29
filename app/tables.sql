679cb3a91b3aceb01bafd21749c5802b48235eb4
Navruz <safabayev@mail.ru>
2022-06-29 14:05:13 +0500

    Table Updated

--- app/tables.sql
+++ app/tables.sql
@@ -1,8 +1,8 @@
-CREATE TYPE ROLE AS ENUM ('admin', 'user');
+CREATE TYPE PAYMENT_TYPE AS ENUM ('daily', 'monthly');
 
 CREATE TABLE IF NOT EXISTS users
 (
-    uuid      UUID PRIMARY KEY,
+    id        UUID PRIMARY KEY,
     firstname VARCHAR        NOT NULL,
     lastname  VARCHAR        NOT NULL,
     phone     VARCHAR UNIQUE NOT NULL,
@@ -10,29 +10,31 @@ CREATE TABLE IF NOT EXISTS users
     password  VARCHAR        NOT NULL
 );
 
+INSERT INTO "users" ("id", "firstname", "lastname", "phone", "gym_name", "password")
+VALUES ('76c2c44c-8fbf-4184-9199-19303a042fa0', 'Admin', 'Adminov', '+998901234567', 'GYM-Forelead',
+        '$s0$e0801$5JK3Ogs35C2h5htbXQoeEQ==$N7HgNieSnOajn1FuEB7l4PhC6puBSq+e1E8WUaSJcGY=');
+
 CREATE TABLE IF NOT EXISTS members
 (
-    uuid         UUID PRIMARY KEY,
-    gym_id       UUID           NOT NULL
-        CONSTRAINT fk_user_id REFERENCES users (uuid) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    firstname    VARCHAR        NOT NULL,
-    lastname     VARCHAR        NOT NULL,
-    phone        VARCHAR UNIQUE NOT NULL,
-    birthday     DATE           NOT NULL,
-    user_picture VARCHAR        NULL
+    id        UUID PRIMARY KEY,
+    user_id   UUID           NOT NULL
+        CONSTRAINT fk_user_id REFERENCES users (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+    firstname VARCHAR        NOT NULL,
+    lastname  VARCHAR        NOT NULL,
+    phone     VARCHAR UNIQUE NOT NULL,
+    birthday  DATE           NOT NULL,
+    image     VARCHAR        NULL
 );
 
 CREATE TABLE IF NOT EXISTS payments
 (
-    uuid       UUID PRIMARY KEY,
-    user_id    UUID      NOT NULL
-        CONSTRAINT fk_user_id REFERENCES users (uuid) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    price      NUMERIC   NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    expired_at TIMESTAMP NOT NULL
+    id           UUID PRIMARY KEY,
+    user_id      UUID         NOT NULL
+        CONSTRAINT fk_user_id REFERENCES users (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+    member_id    UUID         NOT NULL
+        CONSTRAINT fk_member_id REFERENCES members (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+    payment_type PAYMENT_TYPE NOT NULL,
+    price        NUMERIC      NOT NULL,
+    created_at   TIMESTAMP    NOT NULL,
+    expired_at   TIMESTAMP    NOT NULL
 );
-
-INSERT INTO "users" ("uuid", "fullname", "phone", "birthday", "user_picture", "password", "role")
-VALUES ('76c2c44c-8fbf-4184-9199-19303a042fa0', 'Admin Adminov', '+998901237771', '2022-01-01',
-        'images/84385647-4114-4e3f-9caf-358b54f6b955.jpg',
-        '$s0$e0801$5JK3Ogs35C2h5htbXQoeEQ==$N7HgNieSnOajn1FuEB7l4PhC6puBSq+e1E8WUaSJcGY=', 'admin');
