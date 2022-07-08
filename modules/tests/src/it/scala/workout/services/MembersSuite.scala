d09531e3485dbd3c0f7bd96a755efb16c4709cf9
yunusbek1710 <y.yaqubov.1999@gmail.com>
2022-07-08 15:40:57 +0500

    integration tests

--- modules/tests/src/it/scala/workout/services/MembersSuite.scala
+++ modules/tests/src/it/scala/workout/services/MembersSuite.scala
@@ -9,6 +9,8 @@ import eu.timepit.refined.cats.refTypeShow
 import workout.utils.DBSuite
 import workout.utils.Generators.{createMemberGen, defaultUserId, phoneGen}
 
+import java.time.LocalDateTime
+
 object MembersSuite extends DBSuite {
 
   test("Create Member") { implicit postgres =>
@@ -31,6 +33,52 @@ object MembersSuite extends DBSuite {
         )
         members <- members.findByUserId(defaultUserId, 1)
       } yield assert(members.member.contains(member1))
+    }
+  }
+
+  test("Member by id") { implicit postgres =>
+    val messageBroker: MessageBroker[IO] = (messageId: MessageId, phone: Tel, text: String) => IO.unit
+    val members                          = Members[IO](messageBroker, Messages[IO], RedisClient)
+
+    val gen = for {
+      p <- phoneGen
+      m <- createMemberGen(p.some)
+    } yield (p, m)
+    forall(gen) { case (phone, createMember) =>
+      val fileKey = FileKey.unsafeFrom("e8bcab0c-ef16-45b5-842d-7ec35468195e.jpg")
+      for {
+        _              <- members.sendValidationCode(defaultUserId, phone)
+        validationCode <- RedisClient.get(phone.value)
+        member <- members.validateAndCreate(
+          defaultUserId,
+          createMember.copy(code = ValidationCode.unsafeFrom(validationCode.get)),
+          fileKey
+        )
+        memberDB <- members.findMemberById(member.id)
+      } yield assert(memberDB.nonEmpty)
+    }
+  }
+
+  test("Update member active time") { implicit postgres =>
+    val messageBroker: MessageBroker[IO] = (messageId: MessageId, phone: Tel, text: String) => IO.unit
+    val members                          = Members[IO](messageBroker, Messages[IO], RedisClient)
+
+    val gen = for {
+      p <- phoneGen
+      m <- createMemberGen(p.some)
+    } yield (p, m)
+    forall(gen) { case (phone, createMember) =>
+      val fileKey = FileKey.unsafeFrom("e8bcab0c-ef16-45b5-842d-7ec35468195e.jpg")
+      for {
+        _              <- members.sendValidationCode(defaultUserId, phone)
+        validationCode <- RedisClient.get(phone.value)
+        member <- members.validateAndCreate(
+          defaultUserId,
+          createMember.copy(code = ValidationCode.unsafeFrom(validationCode.get)),
+          fileKey
+        )
+        memberDB <- members.updateActiveTime(member.id, LocalDateTime.now())
+      } yield assert(memberDB.id == member.id)
     }
   }
 
