9fcfb17969ed6e34f01f74de4c83b5bc1e3f4ae9
Navruz <safabayev@mail.ru>
2022-07-13 12:23:21 +0500

    Message E2E Tests Improved

--- modules/tests/src/it/scala/e2e/MemberRoutesSuite.scala
+++ modules/tests/src/it/scala/e2e/MemberRoutesSuite.scala
@@ -1,4 +1,5 @@
 package e2e
+import cats.effect.IO
 import com.itforelead.workout.domain._
 import com.itforelead.workout.domain.custom.refinements.ValidationCode
 import com.itforelead.workout.implicits.GenericTypeOps
@@ -10,6 +11,7 @@ import org.http4s.client.dsl.io._
 import org.http4s.headers.`Content-Type`
 import org.http4s.implicits._
 import org.http4s.multipart.{Multipart, Part}
+import weaver.Expectations
 import workout.utils.ClientSuite
 import workout.utils.Generators.{createMemberGen, validationGen}
 
@@ -28,109 +30,72 @@ object MemberRoutesSuite extends ClientSuite {
     }
   }
 
-  test("Validate & Create") { implicit resources =>
+  val fileUrl: Option[URL] = Option(getClass.getResource("/photo_2022-06-28_16-27-20.jpg"))
+
+  def putMemberRequest(
+    shouldReturn: Status,
+    validationCodeIncorrect: Boolean = false,
+    validationCodeExpired: Boolean = false,
+    phoneInUse: Boolean = false,
+    fileUrl: Option[URL] = fileUrl
+  )(implicit resources: Resources[IO]): MemberRoutesSuite.F[Expectations] = {
     forall(createMemberGen()) { createMember =>
       for {
         token <- loginReq.expectAs[JwtToken]
-        _     <- POST(Validation(createMember.phone), uri"/member/sent-code").putHeaders(makeAuth(token)).expectAs[Unit]
-        fileUrl: Option[URL] = Option(getClass.getResource("/photo_2022-06-28_16-27-20.jpg"))
+        _ <-
+          if (validationCodeExpired)
+            IO.unit
+          else POST(Validation(createMember.phone), uri"/member/sent-code").putHeaders(makeAuth(token)).expectAs[Unit]
+
         fileData = fileUrl.map { url =>
           Part.fileData("filename", url, `Content-Type`(MediaType.image.`jpeg`))
         }.toVector
-        code <- resources.redis.get(createMember.phone.value)
-        member    = createMember.copy(code = ValidationCode.unsafeFrom(code.get))
+
+        code <- if (validationCodeExpired) IO.pure(None) else resources.redis.get(createMember.phone.value)
+        member =
+          if (validationCodeIncorrect) createMember
+          else {
+            code match {
+              case Some(value) => createMember.copy(code = ValidationCode.unsafeFrom(value))
+              case None        => createMember
+            }
+
+          }
         multipart = Multipart[F](member.toFormData[F] ++ fileData)
+
+        _ <-
+          if (phoneInUse)
+            PUT(multipart, uri"/member")
+              .withHeaders(multipart.headers)
+              .putHeaders(makeAuth(token))
+              .expectAs[Unit]
+          else IO.unit
+
         result <- PUT(multipart, uri"/member")
           .withHeaders(multipart.headers)
           .putHeaders(makeAuth(token))
-          .expectHttpStatus(Status.Created)
+          .expectHttpStatus(shouldReturn)
       } yield result
     }
+  }
+
+  test("Validate & Create") { implicit resources =>
+    putMemberRequest(Status.Created)
   }
 
   test("Validate & Create: File Part Is Not Defined") { implicit resources =>
-    forall(createMemberGen()) { createMember =>
-      for {
-        token <- loginReq.expectAs[JwtToken]
-        _     <- POST(Validation(createMember.phone), uri"/member/sent-code").putHeaders(makeAuth(token)).expectAs[Unit]
-        fileUrl: Option[URL] = Option(getClass.getResource(""))
-        fileData = fileUrl.map { url =>
-          Part.fileData("filename", url, `Content-Type`(MediaType.image.`jpeg`))
-        }.toVector
-        code <- resources.redis.get(createMember.phone.value)
-        member    = createMember.copy(code = ValidationCode.unsafeFrom(code.get))
-        multipart = Multipart[F](member.toFormData[F] ++ fileData)
-        result <- PUT(multipart, uri"/member")
-          .withHeaders(multipart.headers)
-          .putHeaders(makeAuth(token))
-          .expectHttpStatus(Status.BadRequest)
-      } yield result
-    }
+    putMemberRequest(Status.BadRequest, fileUrl = None)
   }
 
   test("Validate & Create: Validation Code Incorrect") { implicit resources =>
-    forall(createMemberGen()) { createMember =>
-      for {
-        token <- loginReq.expectAs[JwtToken]
-        _     <- POST(Validation(createMember.phone), uri"/member/sent-code").putHeaders(makeAuth(token)).expectAs[Unit]
-        fileUrl: Option[URL] = Option(getClass.getResource("/photo_2022-06-28_16-27-20.jpg"))
-        fileData = fileUrl.map { url =>
-          Part.fileData("filename", url, `Content-Type`(MediaType.image.`jpeg`))
-        }.toVector
-        multipart = Multipart[F](createMember.toFormData[F] ++ fileData)
-        result <- PUT(multipart, uri"/member")
-          .withHeaders(multipart.headers)
-          .putHeaders(makeAuth(token))
-          .expectHttpStatus(Status.NotAcceptable)
-      } yield result
-    }
+    putMemberRequest(Status.NotAcceptable, validationCodeIncorrect = true)
   }
 
   test("Validate & Create: Validation Code Expired") { implicit resources =>
-    forall(createMemberGen()) { createMember =>
-      for {
-        token <- loginReq.expectAs[JwtToken]
-        fileUrl: Option[URL] = Option(getClass.getResource("/photo_2022-06-28_16-27-20.jpg"))
-        fileData = fileUrl.map { url =>
-          Part.fileData("filename", url, `Content-Type`(MediaType.image.`jpeg`))
-        }.toVector
-        multipart = Multipart[F](createMember.toFormData[F] ++ fileData)
-        result <- PUT(multipart, uri"/member")
-          .withHeaders(multipart.headers)
-          .putHeaders(makeAuth(token))
-          .expectHttpStatus(Status.NotAcceptable)
-      } yield result
-    }
+    putMemberRequest(Status.NotAcceptable, validationCodeExpired = true)
   }
 
   test("Validate & Create: Phone In Use") { implicit resources =>
-    val gen = for {
-      m1 <- createMemberGen()
-      m2 <- createMemberGen()
-    } yield (m1, m2)
-    forall(gen) { case (createMember1, createMember2) =>
-      for {
-        token <- loginReq.expectAs[JwtToken]
-        _     <- POST(Validation(createMember1.phone), uri"/member/sent-code").putHeaders(makeAuth(token)).expectAs[Unit]
-        fileUrl: Option[URL] = Option(getClass.getResource("/photo_2022-06-28_16-27-20.jpg"))
-        fileData = fileUrl.map { url =>
-          Part.fileData("filename", url, `Content-Type`(MediaType.image.`jpeg`))
-        }.toVector
-        code1 <- resources.redis.get(createMember1.phone.value)
-        member1    = createMember1.copy(code = ValidationCode.unsafeFrom(code1.get))
-        multipart1 = Multipart[F](member1.toFormData[F] ++ fileData)
-        _ <- PUT(multipart1, uri"/member")
-          .withHeaders(multipart1.headers)
-          .putHeaders(makeAuth(token))
-          .expectAs[Unit]
-
-        member2    = createMember2.copy(code = ValidationCode.unsafeFrom(code1.get), phone = createMember1.phone)
-        multipart2 = Multipart[F](member2.toFormData[F] ++ fileData)
-        result <- PUT(multipart2, uri"/member")
-          .withHeaders(multipart2.headers)
-          .putHeaders(makeAuth(token))
-          .expectHttpStatus(Status.NotAcceptable)
-      } yield result
-    }
+    putMemberRequest(Status.NotAcceptable, phoneInUse = true)
   }
 }
