0b81ef85ab80ac1519ff10963730fe5c415f5c87
OG'ABEK <shomoyxhacker007@mail.ru>
2022-07-29 05:06:24 -0500

    testting deployment scripts added

--- app/nginx.sh
+++ app/nginx.sh
@@ -9,6 +9,17 @@ server {
 	listen [::]:80 default_server;
 
  	server_name trim.uz www.trim.uz;
+ 	  location ~ ^/api/*$ {
+      proxy_pass http://localhost:9000;
+      proxy_http_version 1.1;
+      proxy_set_header X-Real-IP  \$remote_addr;
+      proxy_set_header X-Forwarded-For \$remote_addr;
+      proxy_set_header Host \$host;
+
+      # secure websocket support
+      proxy_set_header Upgrade \$http_upgrade;
+      proxy_set_header Connection "upgrade";
+    }
 }
 EOT
 
